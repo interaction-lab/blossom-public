355dc45d995b1d83a29c89c120c339cee3d570d4
Riya Ranjan <riya.ranjan.00@gmail.com>
2021-01-27 15:55:52 -0800

    clearing for python package setup

--- blossom_web/README.md
+++
@@ -1,8 +0,0 @@
-# blossom_web
-React app for interfacing with blossom
-
-To install dependencies, run
-```
-yarn install
-```
-Typically this is started by running `start.py` from the project root.
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='blossompy',
+    version='0.1',
+    ##packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Necessary ',
+    long_description=open('README.txt').read(),
+    install_requires=['numpy'],
+    url='https://github.com/riya-ranjan/blossom-public',
+    author='Riya Ranjan',
+    author_email='riya.ranjan.00@gmail.com'
+)
