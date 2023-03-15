f64127e52792125849563e0b8f24acddb953d58a
ellenkko <83316093+ellenkko@users.noreply.github.com>
2023-03-15 15:44:32 -0700

    corrected code

--- tracking_file.py
+++ tracking_file.py
@@ -1,5 +1,6 @@
 import csv
-import datetime
+from datetime import datetime
+import time
 
 
 def main():
@@ -7,34 +8,48 @@ def main():
     pause_times = []
     continue_times = []
     fieldnames = ['Start time', 'Pause time(s)', 'Continue time(s)', 'End time']
-    with open('start_stop_times.csv', 'w') as csvfile:
+    with open('start_stop_times.csv', 'a') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(fieldnames)
+        if csvfile.tell() == 0:
+            writer.writerow(fieldnames)
 
     def start():
         st = datetime.now()
-        start_time = now.strftime("%d/%m/%Y %H:%M:%S")
+        start_time = st.strftime("%d/%m/%Y %H:%M:%S")
         session_times.append(start_time)
 
     def end():
         et = datetime.now()
-        end_time = now.strftime("%d/%m/%Y %H:%M:%S")
+        end_time = et.strftime("%d/%m/%Y %H:%M:%S")
         session_times.append(pause_times)
         session_times.append(continue_times)
-        sesion_times.append(end_time)
+        session_times.append(end_time)
         with open('start_stop_times.csv', 'a') as file:
             w = csv.writer(file)
             w.writerow(session_times)
 
     def pause():
         pt = datetime.now()
-        pause_time = now.strftime("%d/%m/%Y %H:%M:%S")
+        pause_time = pt.strftime("%d/%m/%Y %H:%M:%S")
         pause_times.append(pause_time)
 
     def cont():
         ct = datetime.now()
-        continue_time = now.strftime("%d/%m/%Y %H:%M:%S")
+        continue_time = ct.strftime("%d/%m/%Y %H:%M:%S")
         continue_times.append(continue_time)
+
+    start()
+    time.sleep(4)
+    pause()
+    time.sleep(7)
+    cont()
+    time.sleep(6)
+    pause()
+    time.sleep(2)
+    cont()
+    time.sleep(5)
+    end()
+
 
 
 if __name__ == "__main__":
