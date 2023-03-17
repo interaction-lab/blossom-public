f2fb6a14c8738e794cbfe6e236e56b63ced9d3f2
a-oconnell <ao71627@usc.edu>
2023-03-17 16:01:26 -0700

    add aws to log_files

--- study_companions/IdleBehaviors.py
+++ study_companions/IdleBehaviors.py
@@ -3,49 +3,8 @@ sys.path.append("..")
 from blossompy import Blossom
 from time import sleep
 import random
-import sched
 
 from log_session import SessionLogger
-
-# class Idle():
-#     def __init__(self, bl):
-#         self.do_idle = False
-
-#     def start(self):
-#         self.do_idle = True
-#         sc = sched.scheduler()
-#         while self.do_idle:
-#             sc.enter(60, 1, sigh, (sc,))
-#             sc.enter(20, 1, idle_gaze, (sc,))
-#             sc.enter(25, 1, posture_sway, (sc,))
-#             sc.run()
-    
-#     def end(self):
-#         self.do_idle = False
-
-#     def sigh(sc):
-#         bl.motor_goto("all", 90, 1.0)
-#         sleep(2)
-#         bl.motor_goto("all", 0, 1.0)
-
-#     def idle_gaze(sc):
-#         base_position = random.choice([-45, 0, 45])
-#         tower_1_position = random.choice([-90, 0, 90])
-#         bl.motor_goto('base', base_position, 0.5)
-#         bl.motor_goto('tower_1', tower_1_position, 0.5)
-
-#     def posture_sway(sc):
-#         sway_position = random.randrange(3)
-#         bl.motor_goto('tower_1', 0, 0.5)
-#         if sway_position == 0:
-#             bl.motor_goto('tower_2', -45, 0.5)
-#             bl.motor_goto('tower_3', 180, 0.5)
-#         elif sway_position == 1:
-#             bl.motor_goto('tower_2', 180, 0.5)
-#             bl.motor_goto('tower_3', -45, 0.5)
-#         elif sway_position == 2:
-#             bl.motor_goto('tower_2', 0, 0.5)
-#             bl.motor_goto('tower_3', 0, 0.5)
 
 class BlossomController():
     def __init__(self):
@@ -53,7 +12,7 @@ class BlossomController():
         self.bl = Blossom(name="woody")
         self.bl.connect()
 
-        self.logger = SessionLogger("blossom_behavior_log")
+        self.logger = SessionLogger("blossom_behavior_log_")
 
         self.gaze_interval = [15,22]
         self.next_gaze = random.randint(self.gaze_interval[0],self.gaze_interval[1])
@@ -109,51 +68,6 @@ class BlossomController():
         self.bl.motor_goto('tower_3', 100, 0.5)
         self.bl.motor_goto('base', 0, 0.5)
 
-############################################
-# bl = Blossom()
-
-# def main():
-#     """
-#     Start robots, start up server, handle CLI
-#     ToDo: the multi-robot setup should be a seperate file
-#     """
-#     bl.connect() # safe init and connects to blossom and puts blossom in reset position
-
-#     sc = sched.scheduler()
-#     while 1:
-#         sc.enter(60, 1, sigh, (sc,))
-#         sc.enter(20, 1, idle_gaze, (sc,))
-#         sc.enter(25, 1, posture_sway, (sc,))
-#         sc.run()
-    
-
-# def sigh(sc):
-#     print("performing sigh")
-#     bl.motor_goto("all", 90, 1.0)
-#     sleep(2)
-#     bl.motor_goto("all", 0, 1.0)
-
-# def idle_gaze(sc):
-#     base_position = random.choice([-45, 0, 45])
-#     tower_1_position = random.choice([-90, 0, 90])
-#     print("performing idle gaze")
-#     bl.motor_goto('base', base_position, 0.5)
-#     bl.motor_goto('tower_1', tower_1_position, 0.5)
-
-# def posture_sway(sc):
-#     sway_position = random.randrange(3)
-#     print("performing posture sway")
-#     bl.motor_goto('tower_1', 0, 0.5)
-#     if sway_position == 0:
-#         bl.motor_goto('tower_2', -45, 0.5)
-#         bl.motor_goto('tower_3', 180, 0.5)
-#     elif sway_position == 1:
-#         bl.motor_goto('tower_2', 180, 0.5)
-#         bl.motor_goto('tower_3', -45, 0.5)
-#     elif sway_position == 2:
-#         bl.motor_goto('tower_2', 0, 0.5)
-#         bl.motor_goto('tower_3', 0, 0.5)
-
-
-# if __name__ == "__main__":
-#     main()
+    def disconnect(self):
+        self.reset()
+        self.logger.
--- study_companions/log_session.py
+++ study_companions/log_session.py
@@ -1,12 +1,24 @@
 import csv
 from datetime import datetime
-import time
+import socket
+import boto3
+import subprocess
 
-participant_number = 3
+# The rpi name
+rpi_name = socket.gethostname()
+
+# Set up AWS credentials and region
+aws_access_key_id = 'AKIATOJSXW3AQXDXSRNZ'
+aws_secret_access_key = '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'
+region_name = 'us-west-2'
+
+# Set up S3 bucket and file name
+bucket_name = 'study-companions-spring-23'
+
 
 class SessionLogger():
-    def __init__(self, filename = "start_stop_times_p"):
-        self.log_fname = filename + str(participant_number) + ".csv"
+    def __init__(self, filename = "start_stop_times_"):
+        self.log_fname = filename + str(rpi_name) + ".csv"
         fieldnames = ['Event Type', 'Time']
         with open(self.log_fname, 'a') as csvfile:
             writer = csv.writer(csvfile)
@@ -19,77 +31,19 @@ class SessionLogger():
         with open(self.log_fname, 'a') as file:
             w = csv.writer(file)
             w.writerow([event, time])
-        
-# class SessionLogger():
-#     def __init__(self):
-#         log_fname = "start_stop_times_" + str(participant_number) + ".csv"
-#         fieldnames = ['Start time', 'Pause time(s)', 'Continue time(s)', 'End time']
-#         with open(log_fname, 'a') as csvfile:
-#             writer = csv.writer(csvfile)
-#             if csvfile.tell() == 0:
-#                 writer.writerow(fieldnames)
-#         self.start_time = "NULL"
-#         self.end_time = "NULL"
-#         self.pause_start_times = ["NULL"]
-#         self.pause_end_times = ["NULL"]
 
-    
-#     def log_start_time():
-#         global session_times
-#         st = datetime.now()
-#         start_time = st.strftime("%d/%m/%Y %H:%M:%S")
-#         session_times.append(start_time)
+    def store_log(self):
+        print("We will create an S3 client")
+        # Create an S3 client
+        s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id,
+                        aws_secret_access_key=aws_secret_access_key,
+                        region_name=region_name)
+        # Upload log to S3
+        try:
+            print("We are uploading this")
+            s3.upload_file(self.log_fname, bucket_name, self.rpi_name + "/" + self.day + "/" + self.log_fname)
+            print('File ' + self.log_fname + ' uploaded successfully')
+        except Exception as e:
+            print('Error uploading ' + self.log_fname)
+        print("We have called subprocess")
 
-
-
-        
-
-# session_times = []
-# pause_times = []
-# continue_times = []
-
-# def start():
-#     global session_times
-#     st = datetime.now()
-#     start_time = st.strftime("%d/%m/%Y %H:%M:%S")
-#     session_times.append(start_time)
-
-
-# def end():
-#     global session_times, pause_times, continue_times
-#     et = datetime.now()
-#     end_time = et.strftime("%d/%m/%Y %H:%M:%S")
-#     session_times.append(pause_times)
-#     session_times.append(continue_times)
-#     session_times.append(end_time)
-#     with open('start_stop_times.csv', 'a') as file:
-#         w = csv.writer(file)
-#         w.writerow(session_times)
-
-
-# def pause():
-#     global pause_times
-#     pt = datetime.now()
-#     pause_time = pt.strftime("%d/%m/%Y %H:%M:%S")
-#     pause_times.append(pause_time)
-
-
-# def cont():
-#     global continue_times
-#     ct = datetime.now()
-#     continue_time = ct.strftime("%d/%m/%Y %H:%M:%S")
-#     continue_times.append(continue_time)
-
-# def main():
-
-#     fieldnames = ['Start time', 'Pause time(s)', 'Continue time(s)', 'End time']
-#     with open('start_stop_times.csv', 'a') as csvfile:
-#         writer = csv.writer(csvfile)
-#         if csvfile.tell() == 0:
-#             writer.writerow(fieldnames)
-
-
-
-
-# if __name__ == "__main__":
-#     main()
